16ec242642d447f0c028132cbde2af4ac68e968d
Marcos Schloymann <marcosschloymann@gmail.com>
2025-04-06 15:14:20 -0300

    lema

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,6 +11,11 @@ const NavBar = () => {
                     <img src={Logo} className="logo-image img-fluid p-4" alt="templatemo pod talk"/>
                 </a>
 
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
 
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-lg-auto">

151c44352b0b27c9ac2d358a54f65bd72dc9b6d2
Marcos Schloymann <marcosschloymann@gmail.com>
2025-04-21 07:18:18 -0300

    lema

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,112 @@
 import React from 'react'
-import Card from '../components/Card'
 
 const Home = () => {
   return (
     <>
 
       <div className="product-section container">
-       
-          <div className="line-section row d-flex container justify-content-center align-items-center">
+
+        <div className="line-section row d-flex container justify-content-center align-items-center">
           <div className="col-md-4 p-3 card-space">
-            <Card />
+            <div className="contenedor-card">
+              <div className="cardt">
+                <div className="overlay">
+                  <h1 className="text-danger">CONJUNTOS</h1>
+                  <h2>UNISEX</h2>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-md-4 p-3 card-space">
-            <Card />
+            <div className="contenedor-card">
+              <div className="cardt-1">
+                <div className="overlay">
+                  <h1 className="text-danger">CALZAS</h1>
+                  <h2>FEMENINAS</h2>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-md-4 p-3 card-space">
-            <Card />
+            <div className="contenedor-card">
+              <div className="cardt-2">
+                <div className="overlay">
+                  <h1 className="text-danger">TOP LISO</h1>
+                  <h2>FEMENINO</h2>
+                </div>
+              </div>
+            </div>
           </div>
 
+        </div>
+        <div className="line-section row d-flex container justify-content-center align-items-center">
+          <div className="col-md-4 p-3 card-space">
+            <div className="contenedor-card">
+              <div className="cardt-3">
+                <div className="overlay">
+                  <h1 className="text-danger">SHORES</h1>
+                  <h2>MASCULINOS</h2>
+                </div>
+              </div>
+            </div>
           </div>
-       
+          <div className="col-md-4 p-3 card-space">
+            <div className="contenedor-card">
+              <div className="cardt-4">
+                <div className="overlay">
+                  <h1 className="text-danger">ARTICULOS</h1>
+                  <h2>VARIOS</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 p-3 card-space">
+            <div className="contenedor-card">
+              <div className="cardt-5">
+                <div className="overlay">
+                  <h1 className="text-danger">SHORES</h1>
+                  <h2>FEMENINOS</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        {/* <div className="line-section row d-flex container justify-content-center align-items-center">
+          <div className="col-md-4 p-3 card-space">
+            <div className="contenedor-card">
+              <div className="cardt">
+                <div className="overlay">
+                  <h1>title</h1>
+                  <span>descripcion</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 p-3 card-space">
+            <div className="contenedor-card">
+              <div className="cardt">
+                <div className="overlay">
+                  <h1>title</h1>
+                  <span>descripcion</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 p-3 card-space">
+            <div className="contenedor-card">
+              <div className="cardt">
+                <div className="overlay">
+                  <h1>title</h1>
+                  <span>descripcion</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div> */}
+
       </div>
-
-      {/* <Carrousel/> */}
 
 
     </>
